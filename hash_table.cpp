2c40dcabd3ba6966c2d80999f0aad0105cf83361
Thomas Nguyen <Tuancalifornia714@yahoo.com>
2024-10-27 03:04:43 -0700

    is_empty() function implemented

--- hash_table.cpp
+++ hash_table.cpp
@@ -4,3 +4,14 @@ int Hashtable::Hash(int key)
 {
     return key % hash_size;
 }
+
+bool Hashtable::is_empty() const
+{
+    int size = 0;
+    for (int i = 0; i < hash_size; i++)
+    {
+        size += table[i].size();
+    }
+
+    return size == 0;
+}
